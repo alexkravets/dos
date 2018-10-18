672b34c71ff097e32e047cd8c9cd13f786ceb539
Stanislav Demchuk <standemchuk@gmail.com>
2018-10-18 16:50:07 +0300

    Fix Document.save method, add tests

--- test/Document.spec.js
+++ test/Document.spec.js
@@ -5,15 +5,48 @@ const { Profile } = require('test/app/models')
 
 describe('Document', () => {
 
-  before(async() => await Profile.createCollection())
-  after(async() => await Profile.deleteCollection())
+  before(() => Profile.createCollection())
+  after(() => Profile.deleteCollection())
 
   describe('Document.create(context, attributes)', () => {
-
-    it('should use documentSchema as prototype for the new document', async() => {
+    it('uses default values specified in documentSchema', async() => {
       const object = await Profile.create({}, { name: 'Alexander' })
       expect(object.attributes).to.include({ isActive: false })
     })
+  })
 
+  describe('.save(parameters = {})', () => {
+    it('creates a new document', async() => {
+      let object = new Profile()
+      expect(object.id).to.not.exist
+
+      await object.save({ firstName: 'Stas' })
+
+      expect(object.attributes).to.include({ firstName: 'Stas' })
+      expect(object.id).to.exist
+
+      object = await Profile.read({}, { id: object.id })
+      expect(object.attributes).to.include({ firstName: 'Stas' })
+    })
+
+    it('updates an existing document', async() => {
+      let object = await Profile.create({}, { name: 'Alexander' })
+      object._attributes.lastName = 'Kravets'
+
+      await object.save()
+
+      object = await Profile.read({}, { id: object.id })
+      expect(object.attributes).to.include({ lastName: 'Kravets' })
+    })
+
+    it('updates an existing document with specific parameters', async() => {
+      let object = await Profile.create({}, { name: 'Alexander' })
+
+      await object.save({ lastName: 'Kravets' })
+      expect(object.attributes).to.include({ lastName: 'Kravets' })
+
+      object = await Profile.read({}, { id: object.id })
+      expect(object.attributes).to.include({ lastName: 'Kravets' })
+    })
   })
 })
