457e13d124bf06b4e4229ccebb80aa9a6ea68c79
Jyotman Singh <jyotman94@gmail.com>
2021-05-10 16:17:58 +0800

    [Fix] Extract correct name from yaml path (#3)
    This used to fail when the username had a "." in it.

--- src/helpers/loadSync.js
+++ src/helpers/loadSync.js
@@ -5,7 +5,7 @@ const { Schema } = require('@kravc/schema')
 const { readFileSync } = require('fs')
 
 const loadSync = (yamlPath) => {
-  const id     = yamlPath.split('.')[0].split('/').reverse()[0]
+  const id     = yamlPath.split('/').reverse()[0].split('.yaml')[0]
   const source = load(readFileSync(yamlPath))
 
   return new Schema(source, id)
