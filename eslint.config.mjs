2407464605c1a555641e75806c445421d674914b
Oleksandr Kravets <alex@slatestudio.com>
2026-01-27 15:43:12 +0200

    misc: Update dependencies, switch to typescript build

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,55 +1,35 @@
-import globals from "globals";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-import js from "@eslint/js";
-import { FlatCompat } from "@eslint/eslintrc";
+import jsdoc from 'eslint-plugin-jsdoc';
+import eslint from '@eslint/js';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+import { globalIgnores } from 'eslint/config';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
-});
-
-export default [...compat.extends("eslint:recommended"), {
+export default tseslint.config(
+  globalIgnores([ '**/dist/', '**/coverage/' ]),
+  {
     languageOptions: {
-        globals: {
-            ...globals.node,
-            ...globals.mocha,
-        },
-
-        ecmaVersion: 2018,
-        sourceType: "commonjs",
+      globals: {
+        ...globals.node,
+      },
     },
-
+    plugins: {
+      jsdoc,
+    },
     rules: {
-        "comma-style": "error",
-        "consistent-this": ["error", "_this"],
-
-        indent: ["error", 2, {
-            SwitchCase: 1,
-            VariableDeclarator: 2,
-        }],
-
-        "keyword-spacing": "error",
-        "no-multi-spaces": "off",
-        "no-spaced-func": "error",
-        "no-trailing-spaces": "error",
-        quotes: ["error", "single"],
-        semi: ["error", "never"],
-        curly: ["error"],
-        "prefer-arrow-callback": "error",
-        "space-before-blocks": "error",
-
-        "space-before-function-paren": [1, {
-            anonymous: "always",
-            named: "never",
-        }],
-
-        "space-infix-ops": "error",
-        "space-unary-ops": "error",
-        "no-return-await": "error",
-        eqeqeq: "error",
-    },
-}];
+      semi: ['error', 'always'],
+      quotes: ['error', 'single'],
+      'jsdoc/require-jsdoc': ['error', {
+        require: {
+          ArrowFunctionExpression: true,
+          ClassDeclaration: true,
+          ClassExpression: true,
+          FunctionDeclaration: true,
+          FunctionExpression: true,
+          MethodDefinition: true,
+        },
+      }],
+    }
+  },
+  eslint.configs.recommended,
+  tseslint.configs.recommended
+);
---
+++ jest.config.mjs
@@ -0,0 +1,5 @@
+export default {
+  preset: 'ts-jest',
+  testMatch: ['<rootDir>/src/**/*.test.ts'],
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+};
