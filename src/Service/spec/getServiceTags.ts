e7a9d0d7f1f43d8ef4f49902d709a02caf43f48b
Oleksandr Kravets <a@kra.vc>
2026-02-09 16:13:21 +0200

    misc: Revise getServiceTags method

--- src/Service/spec/getServiceTags.ts
+++ src/Service/spec/getServiceTags.ts
@@ -1,4 +1,4 @@
-import { uniq } from 'lodash';
+import { sortBy, uniq } from 'lodash';
 import Operation from '../../Operation';
 
 /** Returns service tags from operations. */
@@ -10,17 +10,10 @@ const getServiceTags = (operations: (typeof Operation)[],) => {
     serviceTags = serviceTags.concat(tags);
   }
 
-  serviceTags = uniq(serviceTags);
-  serviceTags.sort((a, b) => {
-    const aStartsWithUpper = /^[A-Z]/.test(a);
-    const bStartsWithUpper = /^[A-Z]/.test(b);
-
-    if (aStartsWithUpper && !bStartsWithUpper) { return -1; }
-
-    if (!aStartsWithUpper && bStartsWithUpper) { return 1; }
-
-    return a.localeCompare(b);
-  });
+  serviceTags = sortBy(uniq(serviceTags), [
+    (tag) => !/^[A-Z]/.test(tag),
+    (tag) => tag,
+  ]);
 
   const tags = serviceTags.map(name => ({ name }));
 
