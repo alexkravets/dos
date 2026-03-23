b17d834597d5a09cb886af3c817a79b67eb38255
Oleksandr Kravets <alex@slatestudio.com>
2026-03-23 20:46:35 +0200

    Revise spec script

--- bin/spec.js
+++ bin/spec.js
@@ -1,3 +1,22 @@
 #!/usr/bin/env node
-require('ts-node/register');
-require('../scripts/spec.ts');
+'use strict';
+
+const { resolve } = require('path');
+const { statSync } = require('fs');
+
+const [,, servicePath] = process.argv;
+
+if (!servicePath) {
+  process.stderr.write('Usage: spec <path-to-service>\n');
+  process.exit(1);
+}
+
+let fullPath = resolve(process.cwd(), servicePath);
+
+if (statSync(fullPath).isDirectory()) {
+  fullPath = resolve(fullPath, 'index.js');
+}
+
+const { service } = require(fullPath);
+
+process.stdout.write(JSON.stringify(service.spec, null, 2));
